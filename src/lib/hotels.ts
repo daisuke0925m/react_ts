46e03d403685aea89ab64cbbf673224df7054718
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-19 20:45:23 +0900

    upd

--- src/lib/hotels.ts
+++ src/lib/hotels.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { APP_ID, BASE_URL } from 'rakuten/config'
 
-export const searchHotels = async (keyword: string) => {
+export const searchHotels = async (keyword: string): Promise<void> => {
     try {
         const res = await axios.get(BASE_URL + '&applicationId=' + APP_ID + '&keyword=' + keyword)
         console.log(res)
