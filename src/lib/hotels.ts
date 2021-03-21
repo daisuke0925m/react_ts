29960ce38a4567536c2f9036400417372e9a81c2
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2021-03-21 20:55:31 +0900

    upd

--- src/lib/hotels.ts
+++ src/lib/hotels.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { APP_ID, BASE_URL } from 'rakuten/config'
+import { Hotels } from "types/hotels"
 
 export const searchHotels = async (keyword: string): Promise<void> => {
     try {
         const res = await axios.get(BASE_URL + '&applicationId=' + APP_ID + '&keyword=' + keyword)
-        if (res.status == 200) {
-            console.log(res.data)
+        if (res.status === 200) {
+            console.log(res.data.hotels as Hotels)
         }
     } catch (e) {
         console.error(e)
